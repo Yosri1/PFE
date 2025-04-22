316b9c6128bbd85b5c86bdf2819c34d59d680f33
Yosri1 <ZAYANIYOSRI10@GMAIL.COM>
2025-04-22 15:13:49 +0100

    Update text_utils.py

--- utlis/text_utils.py
+++ utlis/text_utils.py
@@ -1,1 +1,8 @@
+# utils/text_utils.py
+import re
 
+def remove_extra_spaces(text):
+    """Remove extra spaces from text."""
+    if text:
+        return re.sub(r'\s{2,}', ' ', text.strip())
+    return None
