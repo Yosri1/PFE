f7d5343d566898ed106ba36cda6515d8d1781585
Yosri1 <ZAYANIYOSRI10@GMAIL.COM>
2025-04-22 15:14:36 +0100

    Update logging_utils.py

--- utlis/logging_utils.py
+++ utlis/logging_utils.py
@@ -1,1 +1,7 @@
+# utils/logging_utils.py
+import logging
 
+def setup_logging():
+    """Set up logging configuration."""
+    logging.basicConfig(level=logging.INFO)
+    return logging.getLogger(__name__)
